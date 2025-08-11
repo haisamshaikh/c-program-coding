260126c772a09e5f0054d0c09693366776e32419
haisamshaikh <hafizhaisam101@gmail.com>
2025-08-11 17:22:33 +0500

    Add files via upload

---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // Library included
+int main() {
+printf("Cyber Security Class\n"); // Output line
+printf("Welcome to C Programming\tLet's Learn!\n");
+printf("%d \n",5+5);
+printf("%d \n",5-5);
+printf("%d \n",5/5);
+printf("%d \n",5*5);
+printf("%d \n",5%5);
+return 0;
+}
